f7f9a638bb92fda293ddfd5512e5d62cdc3e75d8
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2025-03-02 19:28:46 +0530

    Add htop
    need for resource monitoring

--- fedora_android_build_env.sh
+++ fedora_android_build_env.sh
@@ -6,7 +6,7 @@ sudo dnf update -y
 # Install required development tools and dependencies
 sudo dnf install -y git gnupg flex bison make gcc gcc-c++ kernel-devel zip curl \
 zlib-devel glibc-devel glibc-devel.i686 libX11-devel libX11-devel.i686 \
-mesa-libGL-devel libxml2 libxslt unzip fontconfig repo ccache android-tools
+mesa-libGL-devel libxml2 libxslt unzip fontconfig repo ccache android-tools htop
 
 # Confirm installation
 echo "All packages have been installed successfully."
