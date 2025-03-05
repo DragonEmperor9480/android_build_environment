97bdafe6eff371294d4de7fa43ba4b007cb2f9f9
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2025-03-05 22:59:24 +0530

    add openssl

--- fedora_android_build_env.sh
+++ fedora_android_build_env.sh
@@ -7,7 +7,7 @@ sudo dnf update -y
 sudo dnf install -y git gnupg flex bison make gcc gcc-c++ kernel-devel zip curl \
 zlib-devel glibc-devel glibc-devel.i686 libX11-devel libX11-devel.i686 \
 mesa-libGL-devel libxml2 libxslt unzip fontconfig repo ccache android-tools htop \
-gh
+gh openssl
 
 # Confirm installation
 echo "All packages have been installed successfully."
